6d6cf2b7890f9904b020f48e391207a29ea3f731
Peter <Peterweishansen@Hotmail.com>
2023-11-05 19:42:17 +0100

    Slight prep for making tiles invalid for reasons other than being outside the map, added functions to print out a neighbor_info_t struct, the path found by the algorithm, as well as the curreng f-scores of all queue items.
    Tiles now have f-score set to -1 as proxy for being uninitialized, rather than source.
    
    Began work on a function to update position of existing tiles in the queue, if their f-score is better in another way. Needs testing, though.
    
    Misc. other odds and ends.

--- src/a_star.c
+++ src/a_star.c
@@ -22,15 +22,22 @@ typedef struct {
     int x, y;
     int source;
     int g_score_added;
+    int valid;
 } neighbor_info_t;
 
 void initialize_map(tile_t map[MAP_SIZE][MAP_SIZE]);
 
 void initialize_queue();
 
-void initialize_start_tile(tile_t map[MAP_SIZE][MAP_SIZE], int x_start, int y_start);
+tile_t initialize_start_tile(tile_t map[MAP_SIZE][MAP_SIZE], int x_start, int y_start);
 
 void print_tile(tile_t tile);
+
+void print_neighbor_info(neighbor_info_t neighbor_info);
+
+void print_path(tile_t end_tile, tile_t map[MAP_SIZE][MAP_SIZE]);
+
+void print_queue_f_scores();
 
 tile_t queue_fetch_first_element(tile_t map[MAP_SIZE][MAP_SIZE]);
 
@@ -41,6 +48,8 @@ void initialize_h_score(tile_t *tile);
 void update_g_f_scores(tile_t *tile, int new_g_val);
 
 void queue_insert_tile(tile_t new_tile);
+
+void queue_move_updated_tile(tile_t updated_tile, int old_f_score);
 
 int queue_items = 0;
 int x_target, y_target;
@@ -57,19 +66,20 @@ int main() {
     /* Initializing the map, the queue, and the starting tile. */
     initialize_map(map);
     initialize_queue();
-    initialize_start_tile(map, x_start, y_start);
+    map[x_start][y_start] = initialize_start_tile(map, x_start, y_start);
 
     /* Looping over elements in the queue to iterate towards the target. */
     while (queue_items > 0) {
         /* Fetch the first item in the queue, removing them from the queue.*/
+        print_queue_f_scores();
         tile_t current_tile = queue_fetch_first_element(map);
-        print_tile(current_tile);
+        /* print_tile(current_tile); */
 
         /* If the current tile has a h_score of 0, we have reached the target tile
          * and no longer need to iterate over the queue. Instead, reconstruct the
          * path to the target file and print it out. */
         if (current_tile.h_score == 0) {
-            /* TODO, reconstruct path. */
+            print_path(current_tile, map);
             break;
         }
 
@@ -80,7 +90,7 @@ int main() {
 
         for (int i = 0; i < NUM_NEIGHBORS; i++) {
             /*  If the neighbor would be invalid, such as being outside the map, skip it. */
-            if (neighbors_info[i].source == -1) {
+            if (neighbors_info[i].valid == 1) {
                 continue;
             }
 
@@ -92,17 +102,21 @@ int main() {
             /* Check whether the neighboring tile has had scores initialized yet. If not
              * initialize its scores, with the current tile as its source, and insert it
              * into the queue in the appropriate location.
-             * If not, check whether accessing the tile from the current tile would lead
-             * to a lower g-score, and if so, update its values and its queue position. */
-            if (neighbor_tile.source == -1) {
+             * If scores have been initialized, check whether accessing the tile from the
+             * current tile would lead to a lower g-score, and if so, update its values
+             * and its queue position. */
+            if (neighbor_tile.f_score == -1) {
                 neighbor_tile.source = neighbors_info[i].source;
                 initialize_h_score(&neighbor_tile);
                 update_g_f_scores(&neighbor_tile, prelim_g_score);
                 queue_insert_tile(neighbor_tile);
             } else if (neighbor_tile.g_score > prelim_g_score) {
-                continue; /* TODO, also needs to move the old instance of the tile. */
+                int old_f_score = neighbor_tile.g_score;
                 neighbor_tile.source = neighbors_info[i].source;
                 update_g_f_scores(&neighbor_tile, prelim_g_score);
+                queue_move_updated_tile(neighbor_tile, old_f_score);
+                /* TODO, needs testing, but testing needs invalid tile functionality,
+                 * which hasn't been done yet. */
             }
 
             /* Update the map with the information processed above for the
@@ -111,17 +125,18 @@ int main() {
         }
     }
 
-    print_tile(map[x_target][y_target]);
+
+    /* print_tile(map[x_target][y_target]); */
 
     return (0);
 }
 
 void print_tile(tile_t tile) {
     printf("Position: ( %d, %d )\n", tile.x, tile.y);
-    printf("Source: %d\n", tile.source);
-    printf("g-val: %d\n", tile.g_score);
-    printf("h-val: %d\n", tile.h_score);
-    printf("f-val: %d\n", tile.f_score);
+    printf("Source:  %d\n", tile.source);
+    printf("g-score: %d\n", tile.g_score);
+    printf("h-score: %d\n", tile.h_score);
+    printf("f-score: %d\n", tile.f_score);
 }
 
 void print_neighbor_info(neighbor_info_t neighbor_info) {
@@ -131,12 +146,39 @@ void print_neighbor_info(neighbor_info_t neighbor_info) {
     printf("g added: %d", neighbor_info.g_score_added);
 }
 
+void print_path(tile_t end_tile, tile_t map[MAP_SIZE][MAP_SIZE]) {
+    int direction_x[9] = {1, 1, 1, 0, 0, 0, -1, -1, -1};
+    int direction_y[9] = {1, 0, -1, 1, 0, -1, 1, 0, -1};
+    int x, y;
+
+    tile_t path_tile = end_tile;
+
+    printf("End tile:\n");
+    while (path_tile.g_score != 0) {
+        print_tile(path_tile);
+        int source = path_tile.source;
+        x = path_tile.x + direction_x[source];
+        y = path_tile.y + direction_y[source];
+        path_tile = map[x][y];
+    }
+    printf("Start tile:\n");
+    print_tile(path_tile);
+
+}
+
+void print_queue_f_scores() {
+    for (int i = 0; i < queue_items; i++) {
+        printf("%4d ", queue[i].f_score);
+    }
+    printf("\n");
+}
+
 void initialize_map(tile_t map[MAP_SIZE][MAP_SIZE]) {
     for (int i = 0; i < MAP_SIZE; i++) {
         for (int j = 0; j < MAP_SIZE; j++) {
             map[i][j].x = i;
             map[i][j].y = j;
-            map[i][j].source = -1; /* Source is used as a proxy for uninitialized f, g, and h values. */
+            map[i][j].f_score = -1; /* Negative f-score, meaning that the tile has not yet been initialized. */
         }
     }
 }
@@ -147,14 +189,17 @@ void initialize_queue() {
     }
 }
 
-void initialize_start_tile(tile_t map[MAP_SIZE][MAP_SIZE], int x_start, int y_start) {
+tile_t initialize_start_tile(tile_t map[MAP_SIZE][MAP_SIZE], int x_start, int y_start) {
     tile_t start_tile = map[x_start][y_start];
     initialize_h_score(&start_tile);
     update_g_f_scores(&start_tile, 0);
+    start_tile.source = -1;
 
     queue[0] = start_tile;
 
     queue_items++;
+
+    return start_tile;
 }
 
 tile_t queue_fetch_first_element(tile_t map[MAP_SIZE][MAP_SIZE]) {
@@ -171,28 +216,28 @@ tile_t queue_fetch_first_element(tile_t map[MAP_SIZE][MAP_SIZE]) {
 }
 
 void populate_neighbors_info(tile_t current_tile, neighbor_info_t neighbors_info[8]) {
-    /* Setting source to -1 initially, as to be able to distinguish between
-     * neighbors_info which are and are not set by this function. */
+    /* Tiles are assumed to be invalid by default. Valid tiles  will have this
+     * value set to 1 by the program. */
     for (int i = 0; i < NUM_NEIGHBORS; i++) {
-        neighbors_info[i].source = -1;
+        neighbors_info[i].valid = 0;
     }
 
-    int source = 0;
-    int neighbors_info_populated = 0;
+    int source = -1;
+    int neighbor_info_num = 0;
 
-    /* Checking orthogonally and diagonally adjacent positions of the current tile,
-     * making sure to throw out positions which would be located outside the map.
-     * */
+    /* Checking orthogonally and diagonally adjacent positions of the current tile. */
     for (int i = -1; i < 2; i++) {
         for (int j = -1; j < 2; j++) {
+            source++;
             int x_neighbor = current_tile.x + i;
             int y_neighbor = current_tile.y + j;
 
+            /* Skip if the tile would be outside the map. */
             if ((i == 0 && j == 0) || x_neighbor == -1 || y_neighbor == -1
                 || x_neighbor == MAP_SIZE || y_neighbor == MAP_SIZE) {
-                source++;
                 continue;
             }
+            /* TODO, skip if tile is invalid. */
 
             /* Adjacency status, will be 1 if orthogonal and 2 if diagonal.
              * After that, set g scores appropriately.*/
@@ -204,12 +249,11 @@ void populate_neighbors_info(tile_t current_tile, neighbor_info_t neighbors_info
                 g_score_added = G_SCORE_DIAG;
             }
 
-            neighbors_info[neighbors_info_populated].x = x_neighbor;
-            neighbors_info[neighbors_info_populated].y = y_neighbor;
-            neighbors_info[neighbors_info_populated].source = source;
-            neighbors_info[neighbors_info_populated].g_score_added = g_score_added;
-            neighbors_info_populated++;
-            source++;
+            neighbors_info[neighbor_info_num].x = x_neighbor;
+            neighbors_info[neighbor_info_num].y = y_neighbor;
+            neighbors_info[neighbor_info_num].source = source;
+            neighbors_info[neighbor_info_num].g_score_added = g_score_added;
+            neighbor_info_num++;
         }
     }
 }
@@ -253,4 +297,71 @@ void queue_insert_tile(tile_t new_tile) {
     queue[mid] = new_tile;
 
     queue_items++;
+}
+
+void queue_move_updated_tile(tile_t updated_tile, int old_f_score) {
+    /* Changes the position of a tile already in the queue.
+     * First, finds where the tile currently is in the queue,
+     * then figure out where it should be. Bump all elements
+     * between those two points one position back in the queue,
+     * then insert the tile into its new position.*/
+    int low = 0;
+    int high = queue_items;
+    int mid = (int) (high - low) / 2 + low;
+    int f_score_actual = old_f_score;
+
+    while (low < high) {
+        if (queue[mid].f_score < f_score_actual) {
+            low = mid + 1;
+        } else if (queue[mid].f_score > f_score_actual) {
+            high = mid - 1;
+        } else {
+            break;
+        }
+        mid = (int) (high - low) / 2 + low;
+
+    }
+    /* There is a chance that mid is in the middle of a succession of tiles
+     * with identical f-scores to the tile we are updating. Thus, we need to
+     * decrement mid so long as the preceding tile has equal f-score.*/
+    while (queue[mid - 1].f_score == queue[mid].f_score) {
+        mid--;
+        /* Stop the loop if the mid variable reaches 0. Otherwise, the while
+         * condition would try to access something outside the array. */
+        if (mid == 0) {
+            break;
+        }
+    }
+
+    /* Now start at mid and increment until the updated tile has been found.
+     * Before that, set a variable, allowing a check of whether the tile
+     * has been found. */
+    int found = 0;
+    int old_pos;
+    for (int i = mid; queue[i].f_score == old_f_score; i++) {
+        tile_t queue_tile = queue[i];
+        if (queue_tile.x == updated_tile.x && queue_tile.y == updated_tile.y) {
+            found = 1;
+            old_pos = 1;
+            break;
+        }
+    }
+
+    /* Warning in case */
+    if (found == 0) {
+        printf("Tile queue position not updated! Pos: (%d, %d)\n", updated_tile.x, updated_tile.y);
+        return;
+    }
+
+    /* Now move all queue items above the old position in the queue
+     * one place to the left in the queue. */
+    for (int i = old_pos; i <= queue_items; i--) {
+        queue[i] = queue[i + 1];
+    }
+    /* Since we removed an item from the queue, albeit temporarily,
+     * queue_items should be decremented. */
+    queue_items--;
+
+    /* Finally, call the function to insert the updated tile into the queue. */
+    queue_insert_tile(updated_tile);
 }
