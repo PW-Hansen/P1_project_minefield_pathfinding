a695e39fe5ae492ff12d3e201d547b0b9e4f292b
tblank23 <tblank23@student.aau.dk>
2023-11-23 13:59:27 +0100

    Dyn alloc combiner
    Dynamically allocates an array in the `combine_arrays` func.

--- src/combiner/combiner.h
+++ src/combiner/combiner.h
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int size = 64;
+
+void combine_arrays(/*int **terrain_layout, double **kde_layout*/) {
+    double **cost_map = (double **)malloc(size * sizeof(double *));
+
+    for (int i = 0; i < size; i++) {
+        cost_map[i] = (double *)malloc(size * sizeof(double));
+    }
+
+    free(cost_map);
+}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "combiner/combiner.h"
 
 int main() {
+    combine_arrays();
 
     return (0);
 }
