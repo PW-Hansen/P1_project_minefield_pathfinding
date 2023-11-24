60f21b52a313115a174342319f677f6c39c2b64d
Thor <tblank23@student.aau.dk>
2023-11-24 11:52:21 +0100

    Refactor: combiner

--- src/combiner/combiner.h
+++ src/combiner/combiner.h
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 typedef enum {
     HILL = 1,
     ROAD = 2,
@@ -9,37 +6,4 @@ typedef enum {
     FOREST = 5,
 } TileType;
 
-// Temp placeholder variables till merged with settings-feature
-int base_cost = 5;
-int mine_cost = 69;
-int size = 64;
-
-void combine_arrays(int **terrain_layout, double **kde_layout) {
-    // Dynamically allocate the cost map
-    double **cost_map = (double **)malloc(size * sizeof(double *));
-    for (int y = 0; y < size; y++) {
-        cost_map[y] = (double *)malloc(size * sizeof(double));
-    }
-
-    // Fill the cost_map with calculated costs
-    for (int x = 0; x < size; x++) {
-        for (int y = 0; y < size; y++) { 
-            int terrain_cost;
-            switch (terrain_layout[x][y]) {
-                // Temp values which will be changed once merged with settings-feature
-                case HILL:   terrain_cost = 5; break;
-                case ROAD:   terrain_cost = 1; break;
-                case FIELD:  terrain_cost = 3; break;
-                case RIVER:  terrain_cost = 8; break;
-                case FOREST: terrain_cost = 7; break;
-                default:     terrain_cost = 0;
-            }
-
-            cost_map[x][y] = terrain_cost + base_cost + (mine_cost * kde_layout[x][y]);
-            printf("%0.3lf, ", cost_map[x][y]);
-        }
-        printf("\n");
-    }
-
-    free(cost_map);
-}
+void combine_arrays(int **terrain_layout, double **kde_layout);
