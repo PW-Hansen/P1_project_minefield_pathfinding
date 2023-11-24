8d7e10f54674a7c06c472afb6451a512dd0ce20e
Thor <tblank23@student.aau.dk>
2023-11-24 11:04:09 +0100

    x, y instead of i, j

--- src/combiner/combiner.h
+++ src/combiner/combiner.h
@@ -17,15 +17,15 @@ int size = 64;
 void combine_arrays(int **terrain_layout, double **kde_layout) {
     // Dynamically allocate the cost map
     double **cost_map = (double **)malloc(size * sizeof(double *));
-    for (int i = 0; i < size; i++) {
-        cost_map[i] = (double *)malloc(size * sizeof(double));
+    for (int y = 0; y < size; y++) {
+        cost_map[y] = (double *)malloc(size * sizeof(double));
     }
 
     // Fill the cost_map with calculated costs
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) { 
+    for (int x = 0; x < size; x++) {
+        for (int y = 0; y < size; y++) { 
             int terrain_cost;
-            switch (terrain_layout[i][j]) {
+            switch (terrain_layout[x][y]) {
                 // Temp values which will be changed once merged with settings-feature
                 case HILL:   terrain_cost = 5; break;
                 case ROAD:   terrain_cost = 1; break;
@@ -35,8 +35,8 @@ void combine_arrays(int **terrain_layout, double **kde_layout) {
                 default:     terrain_cost = 0;
             }
 
-            cost_map[i][j] = terrain_cost + base_cost + (mine_cost * kde_layout[i][j]);
-            printf("%0.3lf, ", cost_map[i][j]);
+            cost_map[x][y] = terrain_cost + base_cost + (mine_cost * kde_layout[x][y]);
+            printf("%0.3lf, ", cost_map[x][y]);
         }
         printf("\n");
     }
